f14d0641baa379792f925f71b3918f775c733477
Elias Ojala <elkkuve@gmail.com>
2023-05-09 12:07:50 +0300

    fixed problems with dublicate actors

--- app/actors/page.tsx
+++ app/actors/page.tsx
@@ -34,7 +34,7 @@ export default function Page() {
         return (
           <Link
             href={`/movies?actor=${actor.actor_id}`}
-            key={actor.first_name}
+            key={actor.actor_id}
             className="w-52 bg-sakila-10 rounded-lg p-3"
           >
             <h1>{actor.first_name + " " + actor.last_name}</h1>
